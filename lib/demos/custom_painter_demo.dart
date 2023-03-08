f72a73e3be2ae8c50d3e827e1237568c7337ef84
Kin Yuunan <yuunan.kin@gmail.com>
2023-03-09 08:45:36 +0900

    CustomPaint day2

--- lib/demos/custom_painter_demo.dart
+++ lib/demos/custom_painter_demo.dart
@@ -27,7 +27,27 @@ class Polygon extends CustomPainter {
   Polygon({required this.sides});
   @override
   void paint(Canvas canvas, Size size) {
-    // TODO: implement paint
+    final paint = Paint()
+      ..color = Colors.blue
+      ..style = PaintingStyle.stroke
+      ..strokeCap = StrokeCap.round
+      ..strokeWidth = 3;
+    final path = Path();
+    final center = Offset(size.width / 2, size.height / 2);
+    final angle = 2 * pi / sides;
+    final angles = List.generate(sides, (index) => index * angle);
+    final radius = size.width / 2;
+
+    path.moveTo(center.dx + radius * cos(0), center.dy + radius * sin(0));
+    for (final angle in angles) {
+      path.lineTo(
+        center.dx + radius * cos(angle),
+        center.dy + radius * sin(angle),
+      );
+    }
+    path.close();
+
+    canvas.drawPath(path, paint);
   }
 
   @override
