85a99a6e1474a83851b676d9e455b52bb12d108c
Kin Yuunan <yuunan.kin@gmail.com>
2024-03-20 10:17:39 +0900

    make things draggable

--- lib/demos/drag_and_drop_demo.dart
+++ lib/demos/drag_and_drop_demo.dart
@@ -1,6 +1,17 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_practice/shared/demo_widget.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:super_drag_and_drop/super_drag_and_drop.dart';
+
+enum Panel { upper, lower }
+
+typedef PanelLocation = (int, Panel);
+
+extension CopyablePanelLocation on PanelLocation {
+  PanelLocation copyWith({int? index, Panel? panel}) {
+    return (index ?? this.$1, panel ?? this.$2);
+  }
+}
 
 class DragAndDropDemo extends StatefulWidget implements DemoWidget {
   const DragAndDropDemo({super.key});
@@ -49,6 +60,28 @@ class __SplitPanelsState extends State<_SplitPanels> {
   final _spacing = 4.0;
   final List<String> upper = [];
   final List<String> lower = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I'];
+
+  PanelLocation? dragStart;
+  PanelLocation? dropPreview;
+  String? hoveringData;
+
+  void onDragStart(PanelLocation start) {
+    final data = switch (start.$2) {
+      Panel.lower => lower[start.$1],
+      Panel.upper => upper[start.$1],
+    };
+    setState(() {
+      dragStart = start;
+      hoveringData = data;
+    });
+  }
+
+  void updateDrapPreview(PanelLocation update) {
+    setState(() {
+      dropPreview = update;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return LayoutBuilder(
@@ -63,10 +96,12 @@ class __SplitPanelsState extends State<_SplitPanels> {
               height: constraints.maxHeight / 2,
               width: constraints.maxWidth,
               top: 0,
-              child: _ItemPanel(
+              child: _ItemsPanel(
                 crossAxisCount: _columns,
                 items: upper,
                 spacing: _spacing,
+                onDragStart: onDragStart,
+                panel: Panel.upper,
               ),
             ),
             Positioned(
@@ -79,10 +114,12 @@ class __SplitPanelsState extends State<_SplitPanels> {
               height: constraints.maxHeight / 2,
               width: constraints.maxWidth,
               top: constraints.maxHeight / 2 + 2,
-              child: _ItemPanel(
+              child: _ItemsPanel(
                 crossAxisCount: _columns,
                 items: lower,
                 spacing: _spacing,
+                onDragStart: onDragStart,
+                panel: Panel.lower,
               ),
             ),
           ],
@@ -92,16 +129,19 @@ class __SplitPanelsState extends State<_SplitPanels> {
   }
 }
 
-class _ItemPanel extends StatelessWidget {
-  const _ItemPanel({
-    super.key,
+class _ItemsPanel extends StatelessWidget {
+  const _ItemsPanel({
     required this.crossAxisCount,
     required this.items,
     required this.spacing,
+    required this.onDragStart,
+    required this.panel,
   });
   final int crossAxisCount;
   final List<String> items;
   final double spacing;
+  final void Function(PanelLocation) onDragStart;
+  final Panel panel;
 
   @override
   Widget build(BuildContext context) {
@@ -122,7 +162,7 @@ class _ItemPanel extends StatelessWidget {
             ),
           ),
         );
-        return Container(
+        final decoratedContainer = Container(
           height: 200,
           decoration: const BoxDecoration(
             color: Colors.grey,
@@ -130,7 +170,48 @@ class _ItemPanel extends StatelessWidget {
           ),
           child: child,
         );
+        return Draggable(
+          feedback: child,
+          child: _DraggableWidget(
+            data: entry.value,
+            child: decoratedContainer,
+            onDragStart: () {
+              onDragStart((entry.key, panel));
+            },
+          ),
+        );
       }).toList(),
+    );
+  }
+}
+
+class _DraggableWidget extends StatelessWidget {
+  const _DraggableWidget({
+    super.key,
+    required this.data,
+    required this.child,
+    required this.onDragStart,
+  });
+  final String data;
+  final Widget child;
+  final VoidCallback onDragStart;
+
+  @override
+  Widget build(BuildContext context) {
+    return DragItemWidget(
+      child: DraggableWidget(
+        child: child,
+      ),
+      dragItemProvider: (request) {
+        onDragStart();
+        final item = DragItem(localData: data);
+        return item;
+      },
+      allowedOperations: () => [DropOperation.copy],
+      dragBuilder: (context, child) => Opacity(
+        opacity: 0.8,
+        child: child,
+      ),
     );
   }
 }
